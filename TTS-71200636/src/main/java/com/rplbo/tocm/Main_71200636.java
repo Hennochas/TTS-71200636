938d55f1d31dbc2874b032d444d469863757885e
Hennochas <78341298+Hennochas@users.noreply.github.com>
2022-04-04 16:16:13 +0700

    Add files via upload

---
+++ TTS-71200636/src/main/java/com/rplbo/tocm/Main_71200636.java
@@ -0,0 +1,15 @@
+package com.rplbo.tocm;
+
+public class Main_71200636 {
+
+        public static void main(String args[])
+        {
+            int cm = 50;
+            double inch = 0.3937 * cm;
+            double feet = 0.0328 * cm;
+            System.out.printf("Inches is: %.2f \n", inch);
+            System.out.printf("Feet is: %.2f", feet);
+
+        }
+    }
+
